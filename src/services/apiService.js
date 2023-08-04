7e209e9d28d19271c7e4bf2eeb01985fcc4c2aee
Rafael Leyva <rafael.leyva@tiendamia.com>
2023-08-04 01:01:58 -0400

    Refactoring

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,11 +3,11 @@ const F35_API_URL = 'http://devtmia.com/f35/index';
 const apiService = {
   getRobotsStats: async () => {
     const response = await fetch(`${F35_API_URL}/getRobotsStats`);
-    return await response.json();
+    return response.json();
   },
   getRobotsErrorInfo: async () => {
     const response = await fetch(`${F35_API_URL}/getRobotsErrorInfo`);
-    return await response.json();
+    return response.json();
   },
 };
 
