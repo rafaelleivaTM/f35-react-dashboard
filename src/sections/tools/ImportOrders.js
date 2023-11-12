8f16be12e585191c1f192e8b65650415fad06862
Rafael Leyva <rafael.leyva@tiendamia.com>
2023-11-12 17:20:15 -0500

    Working on the import orders tool

--- src/sections/tools/ImportOrders.js
+++ src/sections/tools/ImportOrders.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import {
+  Box,
   Button,
   Card,
   CardContent,
   CardHeader,
   CircularProgress,
+  Collapse,
+  Divider,
   InputAdornment,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
   OutlinedInput,
   Stack,
+  Typography,
 } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
+import { Icon } from '@iconify/react';
 import apiService from '../../services/apiService';
 import Iconify from '../../components/iconify';
 import { parseSearchInput } from '../../utils/functionsUtils';
 import { SOCKET } from '../../App';
+import Scrollbar from '../../components/scrollbar';
 
 const StyledSearch = styled(OutlinedInput, {
   shouldForwardProp: (prop) => prop !== 'hasValue',
@@ -38,22 +49,42 @@ const ImportOrdersForm = () => {
   const inputRef = useRef();
   const [orders, setOrders] = useState('');
   const [loading, setLoading] = useState(false);
-  const [successFullImport, setSuccessFullImport] = useState(false);
+  const [importCompleted, setImportCompleted] = useState(false);
+  const [ordersImportResults, setOrdersImportResults] = useState([]);
+
+  const [open, setOpen] = React.useState({});
+
+  const handleClick = (item) => {
+    setOpen((prevState) => ({ ...prevState, [item]: !prevState[item] }));
+  };
 
   useEffect(() => {
-    SOCKET.on('ImportedOrder', (data) => {
+    const handleImportedOrder = (data) => {
+      setOrdersImportResults((prev) => [...prev, data]);
       console.log(`ImportedOrder event`, data);
-    });
-    SOCKET.on('ImportedOrderFinish', (data) => {
-      console.log(`ImportedOrder event`, data);
-    });
+    };
+
+    const handleImportedOrderFinish = (data) => {
+      console.log(`ImportedOrderFinish event`, data);
+    };
+
+    SOCKET.on('ImportedOrder', handleImportedOrder);
+    SOCKET.on('ImportedOrderFinish', handleImportedOrderFinish);
+
+    // Devuelve una función de limpieza que se ejecutará cuando el componente se desmonte
+    // o antes de que se vuelva a ejecutar el efecto
+    return () => {
+      SOCKET.off('ImportedOrder', handleImportedOrder);
+      SOCKET.off('ImportedOrderFinish', handleImportedOrderFinish);
+    };
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    setSuccessFullImport(false);
+    setImportCompleted(false);
     setLoading(true);
+    setOrdersImportResults([]);
     apiService
       .importOrders(orders)
       .then((response) => {
@@ -61,14 +92,14 @@ const ImportOrdersForm = () => {
         if (response?.data && response.statusCode === 200) {
           setOrders('');
           inputRef.current.querySelector('input').value = '';
-          setSuccessFullImport(true);
+          setImportCompleted(true);
         } else {
-          setSuccessFullImport(false);
+          setImportCompleted(false);
         }
       })
       .catch((error) => {
         console.log('error from importOrders', error);
-        setSuccessFullImport(false);
+        setImportCompleted(false);
       })
       .finally(() => {
         setLoading(false);
@@ -83,6 +114,36 @@ const ImportOrdersForm = () => {
       setOrders(orders);
       inputRef.current.focus();
     }
+    setImportCompleted(false);
+    setOrdersImportResults([]);
+  };
+
+  const importResult = ordersImportResults.reduce((acc, item) => {
+    if (acc[item.order]) {
+      acc[item.order].push({
+        tableName: item.tableName,
+        importResults: item.importResults,
+        error: item.error,
+      });
+    } else {
+      acc[item.order] = [
+        {
+          tableName: item.tableName,
+          importResults: item.importResults,
+          error: item.error,
+        },
+      ];
+    }
+
+    return acc;
+  }, {});
+
+  const getImportFinishMessage = () => {
+    const existErrors = ordersImportResults.some((item) => item.error);
+    if (existErrors) {
+      return 'Import finish with errors';
+    }
+    return 'Import finish';
   };
 
   return (
@@ -92,7 +153,7 @@ const ImportOrdersForm = () => {
         action={
           loading ? (
             <CircularProgress size={30} />
-          ) : successFullImport ? (
+          ) : importCompleted ? (
             <Iconify icon={'eva:checkmark-circle-2-fill'} sx={{ color: 'rgb(64,157,95)' }} />
           ) : (
             <></>
@@ -103,7 +164,7 @@ const ImportOrdersForm = () => {
 
       <CardContent>
         <form onSubmit={handleSubmit}>
-          <Stack direction={'row'} justifyContent={'space-between'}>
+          <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
             <StyledSearch
               ref={inputRef}
               onChange={onChangeInputSearchValue}
@@ -115,11 +176,74 @@ const ImportOrdersForm = () => {
                 </InputAdornment>
               }
             />
-            <Button type="submit" color={'error'} variant="contained" disabled={!orders}>
-              Import
-            </Button>
+            <Box>
+              <Button type="submit" color={'error'} variant="contained" disabled={!orders}>
+                Import
+              </Button>
+            </Box>
           </Stack>
         </form>
+        <Stack direction={'row'} justifyContent={'space-between'} sx={{ my: 3 }}>
+          <Typography variant={'caption'}>
+            {loading ? 'Importing' : ordersImportResults.length === 0 ? 'Start process' : getImportFinishMessage()}
+          </Typography>
+        </Stack>
+
+        <Scrollbar sx={{ height: { xs: 240, sm: 350, lg: 450 } }}>
+          <Stack spacing={1} sx={{ p: 0 }}>
+            {importResult &&
+              Object.keys(importResult).map((order, index) => (
+                <React.Fragment key={order}>
+                  <List
+                    sx={{ width: '100%', bgcolor: 'background.paper' }}
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                    subheader={
+                      <ListSubheader component="div" id="nested-list-subheader">
+                        {order}
+                      </ListSubheader>
+                    }
+                  >
+                    {importResult[order] &&
+                      importResult[order].map((item, indexN) => (
+                        <React.Fragment key={indexN}>
+                          <ListItemButton onClick={() => (item.error ? handleClick(item.tableName) : null)}>
+                            <ListItemIcon>
+                              {item.error ? (
+                                <Iconify icon={'eva:checkmark-circle-2-fill'} sx={{ color: 'rgb(248,29,56)' }} />
+                              ) : (
+                                <Iconify icon={'eva:checkmark-circle-2-fill'} sx={{ color: 'rgb(64,157,95)' }} />
+                              )}
+                            </ListItemIcon>
+                            <ListItemText
+                              primary={`${item.tableName} ${
+                                item?.importResults ? `(${item?.importResults?.join(', ')})` : ''
+                              }`}
+                              size={'small'}
+                            />
+                            {item.error && (
+                              <Icon icon="material-symbols:chevron-right" rotate={open[item.tableName] ? 3 : 1} />
+                            )}
+                          </ListItemButton>
+                          {item.error && (
+                            <Collapse in={open[item.tableName]} timeout="auto" unmountOnExit>
+                              <List component="div" disablePadding>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                  <ListItemText
+                                    primary={`Code: ${item.error.original.code}, Message: ${item.error.original.sqlMessage}`}
+                                  />
+                                </ListItemButton>
+                              </List>
+                            </Collapse>
+                          )}
+                        </React.Fragment>
+                      ))}
+                    {index + 1 < Object.keys(importResult).length && <Divider sx={{ mt: 2 }} />}
+                  </List>
+                </React.Fragment>
+              ))}
+          </Stack>
+        </Scrollbar>
       </CardContent>
     </Card>
   );
